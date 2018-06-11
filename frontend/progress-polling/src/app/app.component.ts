ed5047262af689ff87c5a5bba50516faac80bed9
Kwinten Pisman <kwinten.pisman@gmail.com>
2018-06-11 20:28:35 +0200

    Bad attempt.

--- frontend/progress-polling/src/app/app.component.ts
+++ frontend/progress-polling/src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { interval, Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { filter, map, publishReplay, refCount, share, takeUntil, takeWhile } from 'rxjs/internal/operators';
+
+interface ProgressUpdate {
+  percentage: number;
+  done: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div class="content">
+      <button mat-button (click)="fetchProgress()">Start fetching progress</button>
+      <button mat-button (click)="resetProgress()">Reset progress</button>
+
+      Progress:
+      <mat-progress-bar [value]="progressValue$ | async"></mat-progress-bar>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  progressValue$: Observable<number>;
+
+  constructor(private http: HttpClient) {
+  }
+
+  fetchProgress() {
+    const progressValue$ = interval(1000)
+      .pipe(
+        switchMap(_ => this.http.get<ProgressUpdate>('api/progress')),
+        map(res => res.percentage),
+        publishReplay(1),
+        refCount()
+      );
+
+    this.progressValue$ = progressValue$
+      .pipe(
+        takeUntil(progressValue$.pipe(filter(percentage => percentage > 100)))
+      );
+  }
+
+  resetProgress() {
+    this.http.get('api/progress/reset').subscribe();
+  }
+
 }
--- frontend/progress-polling/src/app/app.module.ts
+++ frontend/progress-polling/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule, MatProgressBarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatProgressBarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
